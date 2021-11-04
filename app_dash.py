4c3234e5e081d9bc26d0a6a1edc5e2e9ee42b17e
Hector <espacha@gmail.com>
2021-11-04 21:29:15 +0100

    commit inicial app dash

---
+++ app_dash.py
@@ -0,0 +1,33 @@
+import dash
+from dash import dcc
+from dash import html
+import plotly.express as px
+import pandas as pd
+
+import api_bme_handler as abh 
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__,
+external_stylesheets=external_stylesheets)
+
+ah = abh.APIBMEHandler(market='IBEX', algo_tag='hgonzalez_algo1')
+data = ah.get_close_data_ticker('SAN')
+
+fig = px.bar(data)
+
+app.layout = html.Div(children=[
+    html.H1(children='Api Handler Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
