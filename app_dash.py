d60654a7e29a1af673305a6c7c2ed1cfea25cb80
Hector <espacha@gmail.com>
2021-11-05 18:06:20 +0100

    paso3

--- app_dash.py
+++ app_dash.py
@@ -14,16 +14,7 @@ app = dash.Dash(__name__,
 external_stylesheets=external_stylesheets)
 
 markets = ['IBEX', 'DAX', 'EUROSTOXX']
-
-ah = abh.APIBMEHandler(market='IBEX')
-
-ticker_master = ah.get_ticker_master()
-tcks = list(ticker_master.ticker)
-ticker_dropdown_values = [{'label': t, 'value': t} for t in tcks]
-
-data = ah.get_close_data_ticker(ticker='SAN')
-
-fig = px.line(data)
+ah = abh.APIBMEHandler(market=markets[0])
 
 app.layout = html.Div(children=[
     html.H1(children='MIAX Data Explorer'),
@@ -44,25 +35,38 @@ app.layout = html.Div(children=[
 
     html.H6("Tickers:"),
         dcc.Dropdown(
-            id='ticker-dropdown',
-            options=ticker_dropdown_values            
+            id='ticker-dropdown',                        
         ),    
 
     dcc.Graph(
-        id='example-graph',
-        figure=fig
+        id='example-graph'        
     )
 ])
 
 @app.callback(
     Output('ticker-dropdown','options'),
     Input('market-dropdown','value'))
-def update_figure(selected_market):
-        ah.market = selected_market
-        ticker_master = ah.get_ticker_master()
-        tcks = list(ticker_master.ticker)
-        ticker_dropdown_values = [{'label': t, 'value': t} for t in tcks]                        
-        return ticker_dropdown_values
+def update_ticker_dropdown(selected_market):
+    ah.market = selected_market
+    ticker_master = ah.get_ticker_master()
+    tcks = list(ticker_master.ticker)
+    ticker_dropdown_values = [{'label': t, 'value': t} for t in tcks]                        
+    return ticker_dropdown_values
+
+@app.callback(
+    Output('ticker-dropdown','value'),
+    Input('ticker-dropdown','options'))
+def update_ticker_dropdown_value(selected_ticker):      
+    return selected_ticker[0]['value']
+
+@app.callback(
+    Output('example-graph','figure'),
+    Input('ticker-dropdown','value'))
+def update_figure(selected_ticker):      
+    data = ah.get_close_data_ticker(ticker=selected_ticker)
+    fig = px.line(data)
+    fig.update_layout(transition_duration=500)
+    return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
